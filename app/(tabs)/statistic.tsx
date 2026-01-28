3ce71e12fee2d1c7a1bab5f7cc59ccf571fc6836
YaroslavD <webdev1232@gmail.com>
2026-01-29 00:27:26 +0300

    добавил вкладку

---
+++ app/(tabs)/statistic.tsx
@@ -0,0 +1,10 @@
+import { CWrapper } from '@/components/ui/CWrapper';
+import { View } from 'react-native';
+
+export default function StatisticPage() {
+    return (
+        <CWrapper>
+            <View>to be continued...</View>
+        </CWrapper>
+    );
+}
